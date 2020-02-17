4c963107b680d2c459f2b64db45e0dbf54c46efd
reddevilcero <reddevil_cero@hotmail.com>
2020-02-17 19:30:50 +0000

    avoiding CONSTANT redifinition

--- bin/environment.rb
+++ bin/environment.rb
@@ -6,13 +6,13 @@ require 'active_record'
 require_relative '../lib/dog'
 require_relative 'sql_runner'
 
-DB = ActiveRecord::Base.establish_connection(
-      :adapter => "sqlite3",
-      :database => "../db/dogs"
-    )
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "../db/dogs"
+)
 
-  DB = ActiveRecord::Base.connection
+DB = ActiveRecord::Base.connection
 
-  if ENV["ACTIVE_RECORD_ENV"] == "test"
-    ActiveRecord::Migration.verbose = false
-  end
+if ENV["ACTIVE_RECORD_ENV"] == "test"
+  ActiveRecord::Migration.verbose = false
+end
